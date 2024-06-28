d658f7fe63957e9b0d51175d4d23d674ae20ec0c
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-06-27 21:35:04 -0300

    create repository

---
+++ src/main/java/dio/spring_aplication_properties/SpringAplicationPropertiesApplication.java
@@ -0,0 +1,13 @@
+package dio.spring_aplication_properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAplicationPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAplicationPropertiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/spring_aplication_properties/SpringAplicationPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.spring_aplication_properties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAplicationPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
